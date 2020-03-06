46392e30076e26df2053ede0001ad44abf5adea0
ntsmoura <natloc215@hotmail.com>
2020-03-06 17:19:20 -0300

    layout filmes

--- TopMovies/app/src/main/java/com/example/topmovies/MainActivity.kt
+++ TopMovies/app/src/main/java/com/example/topmovies/MainActivity.kt
@@ -1,12 +1,48 @@
 package com.example.topmovies
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.view.View
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.movie_layout.*
+import androidx.recyclerview.widget.GridLayoutManager
+import kotlinx.android.synthetic.main.movie_layout.view.*
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+       /* var view = window.decorView
+        view.setBackgroundColor(ContextCompat.getColor(applicationContext, R.color.background))*/
+        recyclerV.apply {
+            //Seta o layout do recycler
+            layoutManager = GridLayoutManager(this@MainActivity,2)
+            val movies = ArrayList<Movie>()
+            movies.add(Movie("a","Batman"))
+            movies.add(Movie("b","Pokemon"))
+            movies.add(Movie("c","Cory na Casa Branca"))
+            movies.add(Movie("c","Cory na Casa Branca"))
+            movies.add(Movie("c","Cory na Casa Branca"))
+            movies.add(Movie("c","Cory na Casa Branca"))
+            movies.add(Movie("c","Cory na Casa Branca"))
+            movies.add(Movie("c","Cory na Casa Branca"))
+            movies.add(Movie("c","Cory na Casa Branca"))
+            movies.add(Movie("c","Cory na Casa Branca"))
+
+            adapter = MovieAdapter(movies)
+
+        }
+
+
     }
+
+    fun movieInfo(view: View){
+        view.textView2.text = "Mudou"
+    }
+
+
 }
---
+++ TopMovies/app/src/main/java/com/example/topmovies/Movie.kt
@@ -0,0 +1,3 @@
+package com.example.topmovies
+
+data class Movie (val banner: String, val title: String)
